ae87f282b9e71ee7d1a2b5c873174fb892e4b953
cclusterpunk <cclusterpunk@gmail.com>
2026-07-22 17:17:05 -0400

    Create .eleventy.js

---
+++ .eleventy.js
@@ -0,0 +1,35 @@
+module.exports = function (eleventyConfig) {
+  // Copy static assets straight through to the output folder
+  eleventyConfig.addPassthroughCopy("css");
+
+  // Group all "character", "location", "faction" etc. pages into one
+  // collection so we can build category listing pages automatically.
+  eleventyConfig.addCollection("wikiPages", function (collectionApi) {
+    return collectionApi.getFilteredByGlob("content/**/*.md");
+  });
+
+  // Build one collection per category tag, e.g. collections.category_character
+  eleventyConfig.addCollection("categories", function (collectionApi) {
+    const pages = collectionApi.getFilteredByGlob("content/**/*.md");
+    const categories = {};
+    pages.forEach((page) => {
+      const cat = page.data.category;
+      if (!cat) return;
+      if (!categories[cat]) categories[cat] = [];
+      categories[cat].push(page);
+    });
+    return categories;
+  });
+
+  return {
+    dir: {
+      input: ".",
+      includes: "_includes",
+      data: "_data",
+      output: "_site",
+    },
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    templateFormats: ["md", "njk", "html"],
+  };
+};
